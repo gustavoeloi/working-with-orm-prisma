ff4a0ebb5d3982732a00e2482005ff24c300e0ed
gustavoeloi <gustavooeloi@gmail.com>
2026-01-02 10:44:23 -0300

    installing lib express async errors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import express, { Request, Response, NextFunction } from "express"
+import express, { Request, Response, NextFunction } from "express";
 
-import { routes } from "./routes"
+import "express-async-errors";
 
-const PORT = 3333
-const app = express()
+import { routes } from "./routes";
 
-app.use(express.json())
-app.use(routes)
+const PORT = 3333;
+const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.use((error: any, request: Request, response: Response, _: NextFunction) => {
-  response.status(500).json({ message: error.message })
-})
+  response.status(500).json({ message: error.message });
+});
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
